61cef0490cce8f53a02d9da1535261f711782ef2
galokpeng <galokpeng@gmail.com>
2025-12-03 19:01:37 +0800

    v1.0.1 add vercel analytics fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { inject } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -17,15 +17,13 @@ const queryClient = new QueryClient({
   },
 });
 
-// 注入Vercel Analytics
-if (import.meta.env.VITE_VERCEL_ANALYTICS_IS_OPEN === "true") {
-  inject();
-}
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      {import.meta.env.VITE_VERCEL_ANALYTICS_IS_OPEN === "true" && (
+        <Analytics />
+      )}
     </QueryClientProvider>
   </StrictMode>
 );
